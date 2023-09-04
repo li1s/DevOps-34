a12f886b43cd2a6583ab3d0d0e3b7734c5634918
Sergei.Lebedev <l1is@yahoo.com>
2023-09-04 10:28:12 +0300

    git-rebase1-update

--- Ветвления в Git/branching/rebase.sh
+++ Ветвления в Git/branching/rebase.sh
@@ -2,12 +2,7 @@
 # display command line options
 
 count=1
-for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
-<<<<<<< HEAD
-
-echo "====="
-=======
->>>>>>> a2ed35f (git-rebase1)
